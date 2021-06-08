0cdd15f8e8c8dfaa33ee6b9db551cc68ea0b3a48
蒲坤 <pukun@offcn.com>
2021-06-08 09:14:28 +0800

    新增几种转换结果, 并减少遍历次数

--- core/array/idmap.go
+++ core/array/idmap.go
@@ -4,76 +4,199 @@ import (
 	"reflect"
 )
 
-type ObjArray struct {
-	value        reflect.Value
-	typ          reflect.Type
-	keyNameValue reflect.Value
-	keyType      reflect.Type
-	elemType     reflect.Type
-	isPtr        bool
-	isMap        bool
+type _field struct {
+	NameValue reflect.Value
+	Type      reflect.Type
 }
 
+type _return struct {
+	Active   bool
+	Out      reflect.Value
+	IsRun    bool
+	KeyIndex int
+}
+
+type ObjArray struct {
+	value         reflect.Value
+	typ           reflect.Type
+	keys          []_field
+	lenKeys       int
+	elemType      reflect.Type
+	elemType2     reflect.Type
+	idMap         _return
+	idMapArray    _return
+	idMapOne      _return
+	idMapOneArray _return
+	idArray       _return
+}
+
+// NewObjArray keyName 默认为 "Id"
 func NewObjArray(data interface{}, keyName interface{}) (res *ObjArray) {
 	res = new(ObjArray)
 	res.value = reflect.ValueOf(data)
 	res.typ = res.value.Type()
-	res.keyNameValue = reflect.ValueOf(keyName)
 	if res.value.Kind() != reflect.Slice {
 		panic("data is not array")
 	}
 	res.elemType = res.typ.Elem()
-	elemType2 := res.elemType
-	if elemType2.Kind() == reflect.Ptr {
-		res.isPtr = true
-		elemType2 = elemType2.Elem()
+	res.elemType2 = res.elemType
+	if res.elemType.Kind() == reflect.Ptr {
+		res.elemType2 = res.elemType.Elem()
 	}
-	switch elemType2.Kind() {
+	res.getKeyType(keyName)
+	return
+}
+
+func (t *ObjArray) getKeyType(name interface{}) {
+	t.lenKeys += 1
+	var key _field
+	key.NameValue = reflect.ValueOf(name)
+	switch t.elemType2.Kind() {
 	case reflect.Struct:
-		key, ok := elemType2.FieldByName(res.keyNameValue.String())
+		field, ok := t.elemType2.FieldByName(key.NameValue.String())
 		if !ok {
-			panic(res.keyNameValue.String() + " is not in struct field")
+			panic(key.NameValue.String() + " is not in struct field")
 		}
-		res.keyType = key.Type
+		key.Type = field.Type
 	case reflect.Map:
-		res.isMap = true
-		res.keyType = elemType2.Elem()
+		key.Type = t.elemType2.Elem()
 	default:
 		panic("elem is not struct or map")
 	}
+	t.keys = append(t.keys, key)
+}
+
+// IdMap 生成{1: struct}
+func (t *ObjArray) IdMap() *ObjArray {
+	t.idMap.Active = true
+	t.idMap.Out = reflect.MakeMap(reflect.MapOf(t.keys[0].Type, t.elemType))
+	return t
+}
+
+// IdMapArray 生成{1: [struct]}
+func (t *ObjArray) IdMapArray() *ObjArray {
+	t.idMapArray.Active = true
+	t.idMapArray.Out = reflect.MakeMap(reflect.MapOf(t.keys[0].Type, reflect.SliceOf(t.elemType)))
+	return t
+}
+
+// IdMapOne 生成{1: 2}
+func (t *ObjArray) IdMapOne(valKey interface{}) *ObjArray {
+	t.getKeyType(valKey)
+	t.idMapOne.Active = true
+	t.idMapOne.KeyIndex = t.lenKeys - 1
+	t.idMapOne.Out = reflect.MakeMap(reflect.MapOf(t.keys[0].Type, t.keys[t.idMapOne.KeyIndex].Type))
+	return t
+}
+
+// IdMapOneArray 生成{1: [1, 2]}
+func (t *ObjArray) IdMapOneArray(valKey interface{}) *ObjArray {
+	t.getKeyType(valKey)
+	t.idMapOneArray.Active = true
+	t.idMapOneArray.KeyIndex = t.lenKeys - 1
+	t.idMapOneArray.Out = reflect.MakeMap(reflect.MapOf(t.keys[0].Type, reflect.SliceOf(t.keys[t.idMapOneArray.KeyIndex].Type)))
+	return t
+}
+
+// IdArray 生成[1, 2, 3]
+func (t *ObjArray) IdArray() *ObjArray {
+	t.idArray.Active = true
+	t.idArray.Out = reflect.MakeSlice(reflect.SliceOf(t.keys[0].Type), 0, 0)
+	return t
+}
+
+func (t *ObjArray) elemGetKey(v reflect.Value, index int) (key reflect.Value) {
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	if v.Kind() == reflect.Map {
+		key = v.MapIndex(t.keys[index].NameValue)
+	} else {
+		key = v.FieldByName(t.keys[index].NameValue.String())
+	}
 	return
 }
 
-func (t *ObjArray) elemGetKey(v reflect.Value) (key reflect.Value) {
-	if t.isPtr {
-		v = v.Elem()
+func (t *ObjArray) run() *ObjArray {
+	for i := 0; i < t.value.Len(); i++ {
+		v := t.value.Index(i)
+		key := t.elemGetKey(v, 0)
+		if t.idMap.Active && !t.idMap.IsRun {
+			t.idMap.Out.SetMapIndex(key, v)
+		}
+		if t.idMapArray.Active && !t.idMapArray.IsRun {
+			if t.idMapArray.Out.MapIndex(key).Kind() == reflect.Invalid {
+				t.idMapArray.Out.SetMapIndex(key, reflect.MakeSlice(reflect.SliceOf(t.elemType), 0, 0))
+			}
+			t.idMapArray.Out.SetMapIndex(key, reflect.Append(t.idMapArray.Out.MapIndex(key), v))
+		}
+		if t.idMapOne.Active && !t.idMapOne.IsRun {
+			t.idMapOne.Out.SetMapIndex(key, t.elemGetKey(v, t.idMapOne.KeyIndex))
+		}
+		if t.idMapOneArray.Active && !t.idMapOneArray.IsRun {
+			if t.idMapOneArray.Out.MapIndex(key).Kind() == reflect.Invalid {
+				t.idMapOneArray.Out.SetMapIndex(key, reflect.MakeSlice(reflect.SliceOf(t.keys[t.idMapOneArray.KeyIndex].Type), 0, 0))
+			}
+			t.idMapOneArray.Out.SetMapIndex(key, reflect.Append(t.idMapOneArray.Out.MapIndex(key), t.elemGetKey(v, t.idMapOneArray.KeyIndex)))
+		}
+		if t.idArray.Active && !t.idArray.IsRun {
+			t.idArray.Out = reflect.Append(t.idArray.Out, key)
+		}
 	}
-	if t.isMap {
-		key = v.MapIndex(t.keyNameValue)
-	} else {
-		key = v.FieldByName(t.keyNameValue.String())
-	}
-	return
+	t.idMap.IsRun = t.idMap.Active
+	t.idMapArray.IsRun = t.idMapArray.Active
+	t.idMapOne.IsRun = t.idMapOne.Active
+	t.idMapOneArray.IsRun = t.idMapOneArray.Active
+	t.idArray.IsRun = t.idArray.Active
+	return t
 }
 
 func (t *ObjArray) ToIdMap() interface{} {
-	resValue := reflect.MakeMap(reflect.MapOf(t.keyType, t.elemType))
-	for i := 0; i < t.value.Len(); i++ {
-		v := t.value.Index(i)
-		resValue.SetMapIndex(t.elemGetKey(v), v)
+	if !t.idMap.Active {
+		t.IdMap()
 	}
-	return resValue.Interface()
+	if !t.idMap.IsRun {
+		t.run()
+	}
+	return t.idMap.Out.Interface()
 }
 
 func (t *ObjArray) ToIdMapArray() interface{} {
-	resValue := reflect.MakeMap(reflect.MapOf(t.keyType, reflect.SliceOf(t.elemType)))
-	for i := 0; i < t.value.Len(); i++ {
-		v := t.value.Index(i)
-		key := t.elemGetKey(v)
-		if resValue.MapIndex(key).Kind() == reflect.Invalid {
-			resValue.SetMapIndex(key, reflect.MakeSlice(reflect.SliceOf(t.elemType), 0, 0))
-		}
-		resValue.SetMapIndex(key, reflect.Append(resValue.MapIndex(key), v))
+	if !t.idMapArray.Active {
+		t.IdMapArray()
 	}
-	return resValue.Interface()
+	if !t.idMapArray.IsRun {
+		t.run()
+	}
+	return t.idMapArray.Out.Interface()
+}
+
+func (t *ObjArray) ToIdMapOne(key ...interface{}) interface{} {
+	if !t.idMapOne.Active {
+		t.IdMapOne(key[0])
+	}
+	if !t.idMapOne.IsRun {
+		t.run()
+	}
+	return t.idMapOne.Out.Interface()
+}
+
+func (t *ObjArray) ToIdMapOneArray(key ...interface{}) interface{} {
+	if !t.idMapOneArray.Active {
+		t.IdMapOneArray(key[0])
+	}
+	if !t.idMapOneArray.IsRun {
+		t.run()
+	}
+	return t.idMapOneArray.Out.Interface()
+}
+
+func (t *ObjArray) ToIdArray() interface{} {
+	if !t.idArray.Active {
+		t.IdArray()
+	}
+	if !t.idArray.IsRun {
+		t.run()
+	}
+	return t.idArray.Out.Interface()
 }
--- test/s2d.go
+++ test/s2d.go
@@ -13,45 +13,59 @@ type A struct {
 
 func test1() {
 	data := []*map[string]int{{
-		"id": 122,
+		"id":  122,
+		"id2": 13,
 	}}
-	arr := array.NewObjArray(data, "id")
+	arr := array.NewObjArray(data, "id").IdMapOne("id2").IdMapOneArray("id2")
 	res := arr.ToIdMap().(map[int]*map[string]int)
 	fmt.Println(res)
 	res2 := arr.ToIdMapArray().(map[int][]*map[string]int)
 	fmt.Println(res2)
+	fmt.Println(arr.ToIdMapOne())
+	fmt.Println(arr.ToIdMapOneArray())
+	fmt.Println(arr.ToIdArray())
 	fmt.Println()
 }
 
 func test2() {
 	data := []map[int]float32{{
 		22: 122,
+		1:  10,
 	}}
-	arr := array.NewObjArray(data, 22)
+	arr := array.NewObjArray(data, 22).IdMapOne(1).IdMapOneArray(1)
 	res := arr.ToIdMap().(map[float32]map[int]float32)
 	fmt.Println(res)
 	res2 := arr.ToIdMapArray().(map[float32][]map[int]float32)
 	fmt.Println(res2)
+	fmt.Println(arr.ToIdMapOne())
+	fmt.Println(arr.ToIdMapOneArray())
+	fmt.Println(arr.ToIdArray())
 	fmt.Println()
 }
 
 func test3() {
 	data := []*A{{1, 2, "a"}, {2, 3, "b"}}
-	arr := array.NewObjArray(data, "Id")
+	arr := array.NewObjArray(data, "Id").IdMapOne("Id2").IdMapOneArray("Id2")
 	res := arr.ToIdMap().(map[int]*A)
 	fmt.Println(res)
 	res2 := arr.ToIdMapArray().(map[int][]*A)
 	fmt.Println(res2)
+	fmt.Println(arr.ToIdMapOne())
+	fmt.Println(arr.ToIdMapOneArray())
+	fmt.Println(arr.ToIdArray())
 	fmt.Println()
 }
 
 func test4() {
 	data := []A{{1, 2, "a"}, {2, 3, "b"}, {3, 2, "aa"}}
-	arr := array.NewObjArray(data, "Id2")
+	arr := array.NewObjArray(data, "Id2").IdMapOne("Name").IdMapOneArray("Name")
 	res := arr.ToIdMap().(map[uint]A)
 	fmt.Println(res)
 	res2 := arr.ToIdMapArray().(map[uint][]A)
 	fmt.Println(res2)
+	fmt.Println(arr.ToIdMapOne())
+	fmt.Println(arr.ToIdMapOneArray())
+	fmt.Println(arr.ToIdArray())
 	fmt.Println()
 }
 
